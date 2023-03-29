f28ffff0f12842f7b9dd36d8e6afe0a448cd90a3
Bri Jackson <71437390+brijax@users.noreply.github.com>
2023-03-29 21:38:49 +0100

    Add Mobile js

---
+++ script.js
@@ -0,0 +1,21 @@
+let menuIcon = document.getElementById("nav-open");
+let navMenu = document.getElementById("menu");
+let backModal = document.querySelector("div.modal")
+let menuClose = document.getElementById("nav-close");
+
+const showMenu = () => {
+navMenu.style.display = "block"
+backModal.style.display = "block"};
+
+console.log(navMenu)
+console.log(backModal)
+
+const closeMenu = () => {
+navMenu.style.display = "none"
+backModal.style.display = "none"};
+
+menuIcon.addEventListener('click', showMenu);
+
+menuClose.addEventListener('click', closeMenu);
+
+backModal.addEventListener('click', closeMenu);
